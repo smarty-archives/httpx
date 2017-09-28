0cd02a972384793543a550229f7d3569a943776e
mob <mob-development@smartystreets.com>
2017-09-28 14:32:25 -0600

    Various convenient getter functions.

--- context.go
+++ context.go
@@ -5,19 +5,41 @@ import (
 	"net/http"
 )
 
-type Contextual http.Request
-
-func (this Contextual) Set(key, value interface{}) {
-	request := http.Request(this)
-	namespace := request.Context().Value(contextNamespace)
-	values := namespace.(map[interface{}]interface{})
-	values[key] = value
-}
-
 func InitializeContext(request *http.Request) *http.Request {
 	parent := request.Context()
 	child := context.WithValue(parent, contextNamespace, make(map[interface{}]interface{}))
 	return request.WithContext(child)
+}
+
+type Contextual http.Request
+
+func (this Contextual) Set(key, value interface{}) {
+	this.getContextValues()[key] = value
+}
+
+func (this Contextual) getContextValues() map[interface{}]interface{} {
+	request := http.Request(this)
+	namespace := request.Context().Value(contextNamespace)
+	return namespace.(map[interface{}]interface{})
+}
+
+func (this Contextual) Int(key string) int {
+	value, _ := this.getContextValues()[key].(int)
+	return value
+}
+func (this Contextual) Int64(key string) int64 {
+	value, _ := this.getContextValues()[key].(int64)
+	return value
+}
+
+func (this Contextual) Uint64(key string) uint64 {
+	value, _ := this.getContextValues()[key].(uint64)
+	return value
+}
+
+func (this Contextual) String(key string) string {
+	value, _ := this.getContextValues()[key].(string)
+	return value
 }
 
 const contextNamespace = "smartystreets"
--- context_test.go
+++ context_test.go
@@ -28,8 +28,41 @@ func (this *ContextFixture) TestContextInitialization() {
 	this.So(actual, should.Resemble, expected)
 }
 
-func (this *ContextFixture) TestContextSetValue() {
+func (this *ContextFixture) TestContextSetValue_ThenAvailableOnContextMap() {
 	Contextual(*this.request).Set("KEY", "VALUE")
 	values := this.request.Context().Value(contextNamespace).(map[interface{}]interface{})
 	this.So(values["KEY"], should.Equal, "VALUE")
+}
+
+func (this *ContextFixture) TestContextGetValue_WhenNotThere_ProvideDefaultValue() {
+	values := Contextual(*this.request)
+
+	this.So(values.Int("KEY"), should.Equal, 0)
+	this.So(values.Int64("KEY"), should.Equal, 0)
+	this.So(values.Uint64("KEY"), should.Equal, 0)
+	this.So(values.String("KEY"), should.Equal, "")
+}
+
+func (this *ContextFixture) TestContextSetValue_Int() {
+	values := Contextual(*this.request)
+	values.Set("INT", int(1))
+	this.So(values.Int("INT"), should.Equal, int(1))
+}
+
+func (this *ContextFixture) TestContextSetValue_Int64() {
+	values := Contextual(*this.request)
+	values.Set("INT64", int64(1))
+	this.So(values.Int64("INT64"), should.Equal, int64(1))
+}
+
+func (this *ContextFixture) TestContextSetValue_Uint64() {
+	values := Contextual(*this.request)
+	values.Set("UINT64", uint64(1))
+	this.So(values.Uint64("UINT64"), should.Equal, uint64(1))
+}
+
+func (this *ContextFixture) TestContextSetValue_string() {
+	values := Contextual(*this.request)
+	values.Set("STRING", "hi")
+	this.So(values.String("STRING"), should.Equal, "hi")
 }
