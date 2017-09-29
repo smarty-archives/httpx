a2d5447cd7657d11f2da96aced918878199795b7
mob <mob-development@smartystreets.com>
2017-09-29 15:22:05 -0600

    Revert "Converted from contextual types to standalone functions"
    This reverts commit c25da7bbed232c670523e8a749a0dca6a0b5acfe.

--- context.go
+++ context.go
@@ -11,32 +11,35 @@ func InitializeContext(request *http.Request) *http.Request {
 	return request.WithContext(child)
 }
 
-func SetContext(request *http.Request, key, value interface{}) {
-	contextValues(request)[key] = value
+type Contextual http.Request
+
+func (this Contextual) Set(key, value interface{}) {
+	this.getContextValues()[key] = value
 }
 
-func GetContext(request *http.Request, key interface{}) interface{} {
-	return contextValues(request)[key]
+func (this Contextual) getContextValues() map[interface{}]interface{} {
+	request := http.Request(this)
+	namespace := request.Context().Value(contextNamespace)
+	return namespace.(map[interface{}]interface{})
 }
-func GetContextInt(request *http.Request, key interface{}) int {
-	value, _ := GetContext(request, key).(int)
+
+func (this Contextual) Int(key string) int {
+	value, _ := this.getContextValues()[key].(int)
 	return value
 }
-func GetContextInt64(request *http.Request, key interface{}) int64 {
-	value, _ := GetContext(request, key).(int64)
-	return value
-}
-func GetContextUint64(request *http.Request, key interface{}) uint64 {
-	value, _ := GetContext(request, key).(uint64)
-	return value
-}
-func GetContextString(request *http.Request, key interface{}) string {
-	value, _ := GetContext(request, key).(string)
+func (this Contextual) Int64(key string) int64 {
+	value, _ := this.getContextValues()[key].(int64)
 	return value
 }
 
-func contextValues(request *http.Request) map[interface{}]interface{} {
-	return request.Context().Value(contextNamespace).(map[interface{}]interface{})
+func (this Contextual) Uint64(key string) uint64 {
+	value, _ := this.getContextValues()[key].(uint64)
+	return value
+}
+
+func (this Contextual) String(key string) string {
+	value, _ := this.getContextValues()[key].(string)
+	return value
 }
 
 const contextNamespace = "smartystreets"
