306f02489c1917ede89ecd895c1b361d8f96f905
mob <mob-development@smartystreets.com>
2017-10-11 16:08:07 -0600

    Removed dead functions.

--- request_functions.go
+++ request_functions.go
@@ -1,26 +1,9 @@
 package httpx
 
 import (
-	"net"
 	"net/http"
 	"sync"
-
-	"github.com/smartystreets/numeric"
 )
-
-func ReadClientIPAddress(request *http.Request) string {
-	if origin := request.Header.Get("X-Forwarded-For"); len(origin) > 0 {
-		return origin
-	} else if address, _, err := net.SplitHostPort(request.RemoteAddr); err == nil {
-		return address
-	} else {
-		return request.RemoteAddr
-	}
-}
-
-func ReadUint64Header(request *http.Request, name string) uint64 {
-	return numeric.StringToUint64(request.Header.Get(name))
-}
 
 func NewWaitGroup(workers int) *sync.WaitGroup {
 	waiter := &sync.WaitGroup{}
@@ -28,8 +11,8 @@ func NewWaitGroup(workers int) *sync.WaitGroup {
 	return waiter
 }
 
-func ReadHeader(request *http.Request, canoicalHeaderName string) string {
-	if values, contains := request.Header[canoicalHeaderName]; contains && len(values) > 0 {
+func ReadHeader(request *http.Request, canonicalHeaderName string) string {
+	if values, contains := request.Header[canonicalHeaderName]; contains && len(values) > 0 {
 		return values[0]
 	} else {
 		return ""
